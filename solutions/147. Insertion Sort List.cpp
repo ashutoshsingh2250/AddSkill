8bba31dabd7d43621eed2a3b0984fd0b3deb161d
ashutoshsingh2250 <ashutoshsingh2250@gmail.com>
2020-11-05 13:08:14 +0530

    56 ms

---
+++ solutions/147. Insertion Sort List.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverse(ListNode * list) {
+        ListNode *prev=NULL;
+        ListNode *temp=NULL;
+        while(list!=NULL) {
+            temp=list->next;
+            list->next=prev;
+            prev=list;
+            list=temp;
+        }
+        return prev;
+    }
+    
+    ListNode* insertionSortList(ListNode* head) { 
+        if(head == NULL) return head;
+        
+        ListNode *list=head;
+        ListNode *root =new ListNode(list->val);
+        ListNode *r, *prev;
+        list=list->next;
+        
+        if(list==NULL) return head;
+        while(list != NULL) {
+            prev=NULL;
+            r=root;
+            while(r->next != NULL && list->val < r->val) {
+                prev=r;
+                r=r->next;
