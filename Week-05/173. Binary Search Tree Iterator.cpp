5a2b503ee654eda60ab9fd99ac0f0d23c75f0665
ashutoshsingh2250 <ashutoshsingh2250@gmail.com>
2020-11-18 19:07:42 +0530

    Update 173. Binary Search Tree Iterator.cpp

--- Week-05/173. Binary Search Tree Iterator.cpp
+++ Week-05/173. Binary Search Tree Iterator.cpp
@@ -1,42 +1,45 @@
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
 class BSTIterator {
-    set<int> v;
-    set<int>::iterator i;
-    
-    void PrintInOrder(TreeNode* node) {
-        if(!node)   return;
-        PrintInOrder(node->left);
-        v.insert(node->val);
-        PrintInOrder(node->right);
-    }
-    
+    stack<TreeNode*> s;
+    
 public:
-    BSTIterator(TreeNode* root) {
-        PrintInOrder(root);
-        i=v.begin();
-    }
-    
-    /** @return the next smallest number */
-    int next() {
-        return *i++;
-    }
-    
-    /** @return whether we have a next smallest number */
-    bool hasNext() {
-        return i!=v.end();
-    }
+    BSTIterator(TreeNode* root) {
+        while (root) {
+            s.push(root);
+            root = root->left;
+        }
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        TreeNode* node = s.top(); s.pop();
+        TreeNode* tmp = node->right;
+        while (tmp) {
+            s.push(tmp);
+            tmp = tmp->left;
+        }
+        return node->val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return !s.empty();
+    }
 };
-​
+
 /**
  * Your BSTIterator object will be instantiated and called as such:
  * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
